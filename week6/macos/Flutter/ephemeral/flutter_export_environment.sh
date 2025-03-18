2362e972b46026481f45f98957070aa3e10f4b89
Mrkhun99 <160562780+Mrkhun99@users.noreply.github.com>
2025-03-18 15:20:14 +0700

    week_6

--- week6/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ week6/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=D:\CADT year 3 term 1\Fundamental of Mobile Development\FLUT\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\CADT year 3 term 2\Advance Mobile Development\Git Folder\Week 6\week6"
+export "FLUTTER_APPLICATION_PATH=D:\CADT year 3 term 2\Advance Mobile Development\Git Folder\week6"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
